a08a275fcccae2fad0c5a0215d0697a7419d98f7
irfanshafee <irfanshafee1@gmail.com>
2024-09-25 21:28:30 +0600

    again trying

--- src/NumWordPrinter.java
+++ src/NumWordPrinter.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class NumWordPrinter {
+public class NumWordPrinter {
+
+    public String printWord(int num, boolean uppercase) {
+        String result;
+
+
+        if (num % 2 == 0 && num % 11 == 0 && num % 13 == 0) {
+            result = "EvenPrimeLucky";
+        } else if (num % 2 == 0 && num % 11 == 0) {
+            result = "EvenPrimeEleven";
+        } else if (num % 2 == 0) {
+            result = "Even";
+        } else if (num % 11 == 0) {
+            result = "PrimeEleven";
+        } else if (num % 13 == 0) {
+            result = "LuckyThirteen";
+        } else {
+            result = "OddOneOut";
+        }
+
+
+        return result;
+    }
+
 }
