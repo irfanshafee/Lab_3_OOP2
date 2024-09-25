b4522cfa3c481b8a41dc45579e10dedb42e9060d
irfanshafee <irfanshafee1@gmail.com>
2024-09-25 22:28:08 +0600

    updating test files

--- src/NumWordPrinterTest.java
+++ src/NumWordPrinterTest.java
@@ -5,6 +5,26 @@ public class NumWordPrinterTest {
 
     NumWordPrinter printer = new NumWordPrinter();
 
+    @Test
+    public void testDivisibleBy2() {
+        assertEquals("Even", printer.printWord(4, false));
+    }
+
+    @Test
+    public void testDivisibleBy11() {
+        assertEquals("EvenPrimeEleven", printer.printWord(22, false));
+    }
+
+
+    @Test
+    public void testDivisibleBy13() {
+        assertEquals("LuckyThirteen", printer.printWord(26, false));
+    }
+
+    @Test
+    public void testDivisibleBy2And11() {
+        assertEquals("EvenPrimeEleven", printer.printWord(22, false));
+    }
 
 
 
